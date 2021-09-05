570de62a2a03ce7cdbbf9379a154076cb0aa5641
Yonathan Uchoa S. Kaveski <yonathan.uchoa@gmail.com>
2021-09-05 16:03:45 -0400

    Room Model + application.properties

---
+++ src/main/java/com/kaveski/yonathan/sala_de_reunioes/model/Room.java
@@ -0,0 +1,31 @@
+package com.kaveski.yonathan.sala_de_reunioes.model;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name="meeting_room")
+class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private LocalDate date;
+    @Column(nullable = true)
+    private LocalDateTime startHour;
+    @Column(nullable = true)
+    private LocalDateTime endHour;
+
+    @Override
+    public String toString(){
+        return "Room [id="+id+",name="+name+",date="+date+",startHour="+startHour+",endHour="+endHour+"]";
+    }
+}
