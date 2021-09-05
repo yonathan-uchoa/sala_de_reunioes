bcab1d027ee30cc9af8d1f7feae71d0eaab591f7
Yonathan Uchoa S. Kaveski <yonathan.uchoa@gmail.com>
2021-09-05 15:50:59 -0400

    Inicio
    projeto inicial puro, gerado pelo star.spring.io com suas depedencias.

---
+++ src/main/java/com/kaveski/yonathan/sala_de_reunioes/SalaDeReunioesApplication.java
@@ -0,0 +1,13 @@
+package com.kaveski.yonathan.sala_de_reunioes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaDeReunioesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaDeReunioesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaveski/yonathan/sala_de_reunioes/SalaDeReunioesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaveski.yonathan.sala_de_reunioes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaDeReunioesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
